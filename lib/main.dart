d00842e939497808768c7e8e4d28364bc3d8a307
s6252410014 <s6252410014@sau.ac.th>
2021-10-22 12:43:06 +0700

    Code V.1 Do fastcall

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_fastcall/views/listfood_detail_ui.dart';
+import 'package:flutter_app_fastcall/views/listfood_new_ui.dart';
+import 'package:flutter_app_fastcall/views/listfood_ui.dart';
+
+main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ListfoodNewUI(),
+      theme: ThemeData(
+        fontFamily: 'Kanit',
+      ),
+    ),
+  );
+}
